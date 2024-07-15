0ae604d5dc96ef19adcd74e7ae2a9e28e499268e
GaneshanHaridoss <ganeshan.haridoss@bounteous.com>
2024-07-15 10:38:14 +0530

    test branch commit

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 
 //Routes
-
+console.log('hello');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
